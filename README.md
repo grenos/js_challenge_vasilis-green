688474bb521c689327d0ed9a212f719142b13fce
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-08-29 14:02:04 +0300

    header tests

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+### Setup
+
+Clone the repo. Install dependencies.
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -7,6 +11,7 @@ In the project directory, you can run:
 ### `npm start`
 
 Runs the app in the development mode.<br>
+
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 ### `npm test`
--- src/components/header/__tests__/header.test.js
+++ src/components/header/__tests__/header.test.js
@@ -1,3 +1,39 @@
-test('should ', () => {
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { findByTestAttr } from '../../../helpers/testUtils'
+import Header from '../Header'
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+
+const setup = (() => {
+  const wrapper = shallow(<Header />)
+  return wrapper
 })
+
+
+test('should render component ', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'header-component')
+  expect(component.length).toBe(1)
+})
+
+test('should render logo', () => {
+  const wrapper = setup()
+  const logo = findByTestAttr(wrapper, 'header-logo')
+  expect(logo.length).toBe(1)
+})
+
+test('should render bag icon', () => {
+  const wrapper = setup()
+  const bag = wrapper.find('BagIcon')
+  expect(bag.length).toBe(1)
+})
+
+
+test('should render favorite icon', () => {
+  const wrapper = setup()
+  const favorite = wrapper.find('FavoriteIcon')
+  expect(favorite.length).toBe(1)
+})
+
